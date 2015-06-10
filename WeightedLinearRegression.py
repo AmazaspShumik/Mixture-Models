3237fa66715ef638143b23e5fd382baa8f96a912
amazaspshaumyan <sha-amazasp@yandex.ru>
2015-06-09 22:48:56 -0700

    fixed bug in parameter estimation

--- WeightedLinearRegression.py
+++ WeightedLinearRegression.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jun  9 13:27:28 2015
-
-@author: amazaspshaumyan
+Weighted Linear Regression , Expert in HME model
 """
 
 import numpy as np
@@ -26,8 +24,6 @@ class WeightedRidgeLinearRegression(object):
     
     def __init__(self,X,Y,weights):
         self.theta        = 0             # coefficients excluding bias term
-        self.beta_0       = np.mean(Y)    # bias term
-        self.lambda_ridge = 0             # regularisation parameter
         self.weights      = weights       # weighting of observations
         self.mse          = 0             # mean squared error
         self.X            = X
@@ -38,35 +34,24 @@ class WeightedRidgeLinearRegression(object):
         '''
         Fits weighted ridge regression 
         '''
-        n,m               = np.shape(X)
-        self.lambda_ridge = lambda_ridge
-        # center data (since bias term is included see excercise 3.5 from ESL)
-        Y_c        =  Y - self.beta_0                                                # centered Y vector
-        mu         =  np.mean(X,axis = 0)
-        X_c        =  X - np.outer(mu,np.ones(m)).T                                  # centered X matrix
-        W          =  np.diagflat(weights)
-        part_one   =  np.dot(np.dot(X_c.T,W),X_c) + lambda_ridge*np.eye(m)
-        part_two   =  np.dot(np.dot(X_c.T,W),Y_c)
-        Theta      =  cholesky_solver_least_squares(part_one, part_two)              # solves least squares with cholesky decomposition
-        mse        =  1/n* np.sqrt(np.dot((Y_c - X_c*Theta).T, ( Y_c - X_c*Theta)))  # mean squared error
-        self.theta =  Theta
-        self.mse   =  mse
-    
+        W = np.diagflat(weights)
+        part_one    = np.dot(np.dot(X.T,W),X)
+        part_two    = np.dot(np.dot(X.T,W),Y)
+        self.theta  = cholesky_solver_least_squares(part_one, part_two)
+        self.mse    =  np.dot((Y - np.dot(X,self.theta)).T, (Y - np.dot(X,self.theta)))
+        
 
     def predict(self,X_test):
         '''
         Predicts target values for X_test
         '''
-        m,n                 = np.shape(X_test)
-        # add bias term
-        X_test_aug          = np.zeros([n,(m+1)])
-        X_test_aug[:,m+1]   = np.ones(n)
-        X_test_aug[:,0:m+1] = X_test
-        Theta_bias          = list(self.theta)
-        Theta_bias          = Theta_bias.append(self.beta_0)
-        # fit
-        fitted              = np.dot(X_test_aug,Theta_bias)
+        fitted = np.dot(X_test,)
         return fitted
+        
+        
+    def get_fitted_params(self):
+        
+        
         
         
         
