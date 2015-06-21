3755c498ffd49c93e7295969fd7ec54f3382432d
amazaspshaumyan <sha-amazasp@yandex.ru>
2015-06-21 01:30:35 -0400

    finished backpropagation

--- Mixture Density Neural Network/nn.py
+++ Mixture Density Neural Network/nn.py
@@ -17,7 +17,7 @@ def d_logistic_sigmoid(X):
 
     
     
-
+from scipy.optimize import fmin_l_bfgs_b
 import numpy as np
 
 class NeuralNetwork(object):
@@ -45,16 +45,9 @@ class NeuralNetwork(object):
         self.out_n     =  n_output
         
         # weighting matrices from input layer to hidden and from hidden to output
-        self.W_in_hi   =  np.zeros([self.in_n,n_hidden])    
-        self.W_hi_out  =  np.zeros([self.hi_n,self.out_n])
-        
-        # activation functions
-        if   activ == "logistic-sigmoid":
-            self.activation = logistic_sigmoid
-        elif activ == "tahn":
-            self.activation = tahn
-            
-        # 
+        self.W_in_hi   =  np.random.random([self.in_n,n_hidden])    
+        self.W_hi_out  =  np.random.random([self.hi_n,self.out_n])
+
         
         
     def forward_propagation(self,X):
@@ -70,7 +63,7 @@ class NeuralNetwork(object):
         # hidden unit activations
         h_act    =  np.dot(X,self.W_in_hi)        
         # hidden layer value
-        H        =  self.activation(h_act)
+        H        =  logistic_sigmoid(h_act)
         # augment hidden layer values with columns of ones (bias term)
         bias_hi  =  np.ones([n,1])        
         H        =  np.concatenate((H,bias_hi), axis = 1)
@@ -88,13 +81,38 @@ class NeuralNetwork(object):
         bias_in   =  np.ones([n,1])
         X         =  np.concatenate((X,bias_in), axis = 1)        
         h_act     =  np.dot(X,self.W_in_hi)        
-        H         =  self.activation(h_act)
+        H         =  logistic_sigmoid(h_act)
         bias_hi   =  np.ones([n,1])        
         H         =  np.concatenate((H,bias_hi), axis = 1)
+        o_act     =  np.dot(H,self.W_hi_out)
         
         # errors in output layer
-        err_out   = ( Y - H ).T
-        err_hi    =  d_logistic_sigmoid(h_act).T * np.dot(self.W_hi_out,err_out)
+        err_out    =  ( Y - o_act ).T
+        # exclude bias term to propagate error to hidden layer
+        W_hi_out_b =  self.W_hi_out[0:self.hi_n-1,:]
+        # error  of hidden layer
+        print np.shape(d_logistic_sigmoid(h_act).T)
+        print np.shape(np.dot(W_hi_out_b.T,err_out))
+        err_hi     =  d_logistic_sigmoid(h_act).T * np.dot(W_hi_out_b.T,err_out)
+        # gradient for weights from hidden to output layer
+        grad_hi_out  =  np.dot(err_out,H)
+        # gradient for weights from input to hidden layer
+        grad_in_hi   =  np.dot(err_hi,X)
+        print grad_hi_out
+        print grad_in_hi
+        
+        
+if __name__=="__main__":
+    nnet = NeuralNetwork(2,2,1)
+    X = np.zeros([60,2])
+    X[:,0] = np.linspace(0,4,60)
+    X[:,1] = np.random.random(60)
+    Y = np.sin(X)
+    #print nnet.forward_propagation(X)
+    nnet._backprop_train(X,Y)
+        
+        
+        
         
         
         
