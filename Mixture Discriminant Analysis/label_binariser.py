c554995ee954ab6b9987e3943c06609b0e3cf23f
amazaspshaumyan <sha-amazasp@yandex.ru>
2015-06-24 22:48:08 -0700

    new comments , small fixes

--- Mixture Discriminant Analysis/label_binariser.py
+++ Mixture Discriminant Analysis/label_binariser.py
@@ -28,6 +28,8 @@ class LabelBinariser(object):
     Allows easily transform vector of targets for classification to ground truth 
     matrix and easily make inverse transformation.
     
+    n = n_samples , k = n_classes
+    
     Parameters:
     ------------
     
@@ -65,7 +67,15 @@ class LabelBinariser(object):
         ------------
                 
         compress: bool
-                  If True will use csr_matrix to output compressed matrix
+               If True will use csr_matrix to output compressed matrix
+                  
+        Returns:
+        --------
+        
+        Y: numpy array of size 'n x k'
+               Ground truth matrix , column number represents class index,
+               each row has all zeros and only one 1.  
+                
         '''
         Y = np.zeros([self.n,self.k])
         for el,idx in self.direct_mapping.items():
@@ -82,7 +92,13 @@ class LabelBinariser(object):
         Parameters:
         -----------
         compressed: bool
-            If True input is csr_matrix, otherwise B is numpy array
+             If True input is csr_matrix, otherwise B is numpy array
+            
+        Returns:
+        ---------
+        
+        Y: numpy array of size 'n x 1'
+            Vector of targets, classes
         '''
         if compressed is True:
             B = B.dot(np.eye(np.shape(B)[1]))
@@ -90,6 +106,30 @@ class LabelBinariser(object):
         for i in range(np.shape(B)[1]):
             Y[B[:,i]==1] = self.inverse_mapping[i]
         return Y
+        
+        
+    def convert_prob_matrix_to_vec(self,Y):
+        '''
+        Converts matrix of probabilities to vector of classification targets
+        
+        Parameters:
+        -----------
+        Y:  numpy array of size [n_samples,n_classes]
+            Matrix of class probabilities, element at cell [i,j] shows probability
+            that observation i belongs to class j
+            
+        Returns:
+        --------
+        
+        Y: numpy array of size 'n x k'
+            Ground truth matrix , column number represents class index,
+            each row has all zeros and only one 1.
+            
+        '''
+        Y_max = np.argmax(Y, axis = 1)
+        Y     = np.array([self.inverse_mapping[e] for e in Y_max])
+        return Y
+        
         
         
         
